a3a99fc4bc1e8c5d8183aa5a7a6a2ca2746f229e
bubibubi <u.ballestrazzi@gmail.com>
2024-02-16 11:18:26 +0100

    Aggiungere i file di progetto.

---
+++ RettangoloParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RettangoloParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo r = new Rettangolo(5, 3);
+            Rettangolo r2;
+            r2 = Rettangolo.Parse(r.ToString());
+            Console.WriteLine(r2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RettangoloParse/Rettangolo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RettangoloParse
+{
+    internal class Rettangolo
+    {
+        public int L1 { get; set; }
+        public int L2 { get; set; }
+
+        public Rettangolo(int l1, int l2)
+        {
+            this.L1 = l1;
+            this.L2 = l2;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", L1, L2);
+        }
+
+        public static Rettangolo Parse(string s)
+        {
+            string[] parts = s.Split(';');
+            return new Rettangolo(int.Parse(parts[0]), int.Parse(parts[1]));    
+        }
+    }
+}
